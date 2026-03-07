1f3577959cc98e0c1be0a1526d7a3fd6480044f5
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-03-07 16:28:37 +0530

    [Shivam] : added AddressBookAppApplicationTests file

---
+++ src/test/java/com/addressbookapp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressbookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
