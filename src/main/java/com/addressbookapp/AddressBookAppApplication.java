9f8da9aef9ac5e6609cc8f123dbd8dbc88d3ef0d
HackHeal-1828191 <cy.shivam.m@gmail.com>
2026-03-07 23:15:06 +0530

    [Shivam] : Implemented UC3 - Updated AddressBookAppApplication as per requirement

--- src/main/java/com/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/addressbookapp/AddressBookAppApplication.java
@@ -1,13 +1,32 @@
+
 package com.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.addressbookapp.model.Contact;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressBookAppApplication.class, args);
+
+// You can temporarily test(feature/uc-02-add-contact-console) it inside AddressBookApplication:
+//
+//		Contact contact = new Contact(
+//		        "Shivam",
+//		        "Mishra",
+//		        "ABC Street",
+//		        "Bhopal",
+//		        "MP",
+//		        "462002",
+//		        "9999999999",
+//		        "shivam@gmail.com"
+//		);
+//
+//		System.out.println(contact);
+		
 	}
 
 }
